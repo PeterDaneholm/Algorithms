068f66fd8e8a552aa3f5b10c716f01367eadfb90
PeterDaneholm <av28qm@student.aau.dk>
2025-01-28 19:42:00 +0100

    Couple comments

--- Algorithms/Arrays/DSALinearSearch.cs
+++ Algorithms/Arrays/DSALinearSearch.cs
@@ -10,12 +10,14 @@ public class DSALinearSearch
     {
         for (int i = 0; i < allNums.Length; i++)
         {
+            //Return the first occurrence of the target
             if (allNums[i] == target)
             {
                 Console.WriteLine($"Match found at index {i}");
                 return i;
             }
         }
+        //Otherwise we'll return -1 indicating it wasn't found
         Console.WriteLine($"Match not found, you get a -1");
         return -1;
     }
