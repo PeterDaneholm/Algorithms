277cfc78c2f17354e41637a718351735f021b799
PeterDaneholm <av28qm@student.aau.dk>
2025-03-30 17:25:31 +0200

    Finished on Hash tables, on to sets

--- Algorithms/HashTables/HashTables.cs
+++ Algorithms/HashTables/HashTables.cs
@@ -21,7 +21,8 @@ public class HashTables
     //use a Hash Set, which is a simplified version of a Hash Table, or a regular Hash Table
 
     public HashSet<int> allNumsHashSet = new HashSet<int>(); //C# standard implementation here
-    //Step 2
+    //Step 2 - Create Hash Function
+    // A hash function takes the key of an argument to generate a hash code, which indicates which bucket the element belongs to. 
     //We'll now create the hash function, which should place the values in the right place of the array
     //In the example, this is done by converting the string values into a unicode number (numerical value), and a subsequent
     //modulo operator with 10 is performed on that number. This leaves an index they can be placed in: https://www.w3schools.com/dsa/dsa_theory_hashtables.php
@@ -36,6 +37,44 @@ public class HashTables
     
     //Step 4 - Handling Collision
     /*
-     * 
+     * In instances, where we want to insert a new value into our hashset, but the index is already taken, we'll need to
+     * we can make more room in the bucket (specific index) for the new value. This is called 'Chaining', by implementing each
+     * bucket as an array or linked list.
+     * This can look like this:
+     * public static int[] hashSetWithArrays = [
+     *   [None], <-- each are individual buckets
+     *   ['Jones'],
+     *   [None],
+     *   ['Lisa', 'Stuart'], <-- Here we've added the new value with the same index value after hashing
+     *   [None],
+     *   ...
+     * ]
+     */
+    
+    //Step 5 - Example
+    //...
+    
+    
+    //Uses for a Hash Table
+    /*
+     * Hash tables are particularly great for:
+     *   - Checking if an item is in the collection
+     *   - Storing unique items and quickly locating them
+     *   - Connecting values to keys (e.g. linking names to phone numbers)
+     *
+     * The most common advantage is the time complexity of a hash table. It has the complexity O(1), while arrays and
+     * linked lists have the complexity of O(n)
+     */
+    
+    //Hash Set vs Hash Map
+    /*
+     * Hash Set:
+     *  - Every item is a unique key
+     *  - Check if element is in set
+     *  - Average time complexity of O(1)
+     * Hash Map:
+     *  - Every entry is a key-value pair, with the key being unique
+     *  - Finding information is based on the key
+     *  - Average time complexity of O(1)
      */
 }
