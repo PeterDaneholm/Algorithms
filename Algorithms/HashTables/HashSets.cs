e2340a2f108e3f04953f03334d631ceb713cdaa1
PeterDaneholm <av28qm@student.aau.dk>
2025-03-30 18:21:19 +0200

    Small theory on hash set, should try to implement an example of such

---
+++ Algorithms/HashTables/HashSets.cs
@@ -0,0 +1,19 @@
+namespace Algorithms;
+
+public class HashSets
+{
+    /* Hash Sets
+     * A Hash Set is a type of Hash Table that usually holds a larger number of elements
+     * Search, add, and removing elements are still very fast though
+     *
+     * A Hash set cannot have more than 1 element with the same value!
+     *
+     * We can run into scenarios, where the hash function places all the values in the same bucket, which is definitely not ideal.
+     * In worst case scenarios, we can end up with a time complexity of O(n), which is the same as for arrays and linked lists.
+     * Therefore it's very important to design the hash function so it will distribute the values into all the buckets.
+     * Also remember to utilize all buckets and only have about as many as hash set elements, as having too many is a waste of memory and having too few is a waste of time. 
+     */
+    
+    //Example
+    
+}
